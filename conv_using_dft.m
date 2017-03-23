c0b4f118c82b2561ec959c56cfc7953dd9c94ac2
melvinFelix <mfelix366@gmail.com>
2017-03-23 14:39:44 +0530

    Add linear convolution using dft and idft

---
+++ conv_using_dft.m
@@ -0,0 +1,19 @@
+%La Experimente sexto
+%Linear convolution using dft and idft
+function[y] = conv_using_dft(x_in, h_in)
+
+len_y = length(x_in) + length(h_in) - 1;
+
+x = [x_in zeros(1,(len_y - length(x_in)))];
+h = [h_in zeros(1,(len_y - length(h_in)))];
+
+X = dft_mf(x,len_y);
+H = dft_mf(h,len_y);
+
+Y = X.*H;
+
+y = round(idft_mf(Y,len_y));
+
+end
+
+
